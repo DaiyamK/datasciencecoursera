37f9e254749b6cb86adea9ad1ed7aa00ac162129
RameenM <rameenkamran19@gmail.com>
2020-10-16 02:39:00 -0700

    Initiate commit of hellowworld.R

---
+++ helloworld.R
@@ -0,0 +1,5 @@
+print("This file was created within RStudio")
+
+print("And now it lives on GitHub")
+git config --global user.email "rameenkamran19@gmail.com"
+git config --global user.name "RamenM"
